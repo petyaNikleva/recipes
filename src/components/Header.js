de1f1d6588735dd0afd8d34b4aa26a2fd524bd88
P N <petyanikleva@yahoo.com>
2021-12-03 18:33:16 +0200

    Implement guest/user navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,71 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-function Header () {
+import { AuthContext } from "../context/AuthContext.js"
+
+function Header() {
+    const { user } = useContext(AuthContext);
+
+    let guestNavigation = (
+        <>
+            <li className="active"><Link to="/">НАЧАЛО</Link></li>
+            <li><Link to="/about">ЗА НАС</Link></li>
+            <li><Link to="/login">ВХОД</Link></li>
+            <li><Link to="/register">РЕГИСТРАЦИЯ</Link></li>
+            <li><Link to="/recipes">РЕЦЕПТИ</Link></li>
+            <li><Link to="/gallery">ГАЛЕРИЯ</Link></li>
+            <li><Link to="/menu">МЕНЮ</Link></li>
+        </>
+    );
+
+    let userNavigation = (
+        <>
+            <li className="active"><Link to="/">НАЧАЛО</Link></li>
+            <li><Link to="/about">ЗА НАС</Link></li>
+            <li><Link to="/recipes">РЕЦЕПТИ</Link></li>
+            <li><Link to="/create">СЪЗДАЙ РЕЦЕПТА</Link></li>
+            <li><Link to="/gallery">ГАЛЕРИЯ</Link></li>
+            <li><Link to="/menu">МЕНЮ</Link></li>
+            <li><Link to="/logout">ИЗХОД</Link></li>
+
+
+        </>
+    )
+
     return (
         <div id="site-header">
-        <header id="header" className="header-block-top fixed-menu">
-          <div className="container">
-            <div className="row">
-              <div className="main-menu">
-                <nav className="navbar navbar-default" id="mainNav">
-                  <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                      <span className="sr-only">Toggle navigation</span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                    </button>
-                    <div className="logo">
-                      <a className="navbar-brand js-scroll-trigger logo-header" href="#">
-                        <img src="images/logo.png" alt="" />
-                      </a>
+            <header id="header" className="header-block-top fixed-menu">
+                <div className="container">
+                    <div className="row">
+                        <div className="main-menu">
+                            <nav className="navbar navbar-default" id="mainNav">
+                                <div className="navbar-header">
+                                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                        <span className="sr-only">Toggle navigation</span>
+                                        <span className="icon-bar"></span>
+                                        <span className="icon-bar"></span>
+                                        <span className="icon-bar"></span>
+                                    </button>
+                                    <div className="logo">
+                                        <a className="navbar-brand js-scroll-trigger logo-header" href="#">
+                                            <img src="images/logo.png" alt="" />
+                                        </a>
+                                    </div>
+                                </div>
+                                <div id="navbar" className="navbar-collapse collapse">
+                                    <ul className="nav navbar-nav navbar-right">
+                                        {user.username
+                                            ? userNavigation
+                                            : guestNavigation
+                                        }
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
                     </div>
-                  </div>
-                  <div id="navbar" className="navbar-collapse collapse">
-                    <ul className="nav navbar-nav navbar-right">
-                      <li className="active"><Link to="/">НАЧАЛО</Link></li>
-                      <li><Link to="/login">ВХОД</Link></li>
-                      <li><Link to="/register">РЕГИСТРАЦИЯ</Link></li>
-                      <li><Link to="/recipes">РЕЦЕПТИ</Link></li>
-                      <li><Link to="/create">СЪЗДАЙ РЕЦЕПТА</Link></li>
-                      <li><Link to="/about">ЗА НАС</Link></li>
-                      <li><Link to="/gallery">ГАЛЕРИЯ</Link></li>
-                      <li><Link to="/menu">МЕНЮ</Link></li>
-                    </ul>
-                  </div>
-                </nav>
-              </div>
-            </div>
-          </div>
-        </header>
-      </div>
+                </div>
+            </header>
+        </div>
     )
 }
 
