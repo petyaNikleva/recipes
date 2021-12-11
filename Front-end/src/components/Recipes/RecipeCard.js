cec7bb5b19cd3175e60ffa80ee40a082cd38fa15
P N <petyanikleva@yahoo.com>
2021-12-11 23:41:00 +0200

    add css file to RecipeCard

--- Front-end/src/components/Recipes/RecipeCard.js
+++ Front-end/src/components/Recipes/RecipeCard.js
@@ -1,3 +1,5 @@
+import './RecipeCard.css';
+
 import { Link } from 'react-router-dom';
 
 function RecipeCard ({recipe}) {
@@ -5,15 +7,15 @@ function RecipeCard ({recipe}) {
         <div className="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">
         <div className="col-md-6 col-sm-6">
           <div className="blog-block">
-            <div className="blog-img-box">
-              <img src={recipe.img} alt="" style={{width: '250px', height: '200px'}}/>
+            <div className="blog-img-box col-lg-6 col-md-6">
+              <img src={recipe.img} alt="" style={{width: '270px', height: '200px', transform: "translate(-15px, 0px)"}}/>
               <div className="overlay">
                 <Link to={`/recipes/details/${recipe._id}`}><i className="fa fa-link" aria-hidden="true"></i></Link>
               </div>
             </div>
-            <div className="blog-dit">
+            <div className="blog-info col-lg-6 col-md-6">
               <p><span>{recipe.type}</span></p>
-              <h2>{recipe.name}</h2>
+              <h3 className='blog-info-h2'>{recipe.name}</h3>
               <h5>НИВО НА ТРУДНОСТ: {recipe.difficulty}</h5>
             </div>
           </div>
