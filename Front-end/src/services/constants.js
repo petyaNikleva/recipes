e3b3c0c9a3253530223f633a3c4667e7733bcf9a
P N <petyanikleva@yahoo.com>
2023-08-04 19:45:32 +0300

    change BE URL

--- Front-end/src/services/constants.js
+++ Front-end/src/services/constants.js
@@ -1,3 +1,3 @@
 // const baseUrl = 'http://localhost:5000';
 // const baseUrl = 'https://rest-api-recipes.herokuapp.com';
-export const baseUrl = 'https://tender-waders-colt.cyclic.cloud';
+export const baseUrl = 'https://recipes-api-yg7u.onrender.com';
